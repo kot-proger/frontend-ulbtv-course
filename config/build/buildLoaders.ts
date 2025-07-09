54c7384d1212bc675bfea051cb38835cd614c1a9
kot-proger <babecaleksei@yandex.ru>
2025-07-09 16:31:39 +0700

    add bable and i18 extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -19,6 +19,18 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: ['file-loader'],
   };
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [['i18next-extract', { nsSeparator: '~', locales: ['ru', 'en'], keyAsDefaultValue: true }]],
+      },
+    },
+  };
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -39,5 +51,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  return [typescriptloader, cssLoader, svgLoader, fileLoader];
+  return [fileLoader, svgLoader, babelLoader, typescriptloader, cssLoader];
 }
