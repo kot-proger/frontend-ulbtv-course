2a7b5224f6467ae17f03e34aa8697f539c73ef5e
kot-proger <babecaleksei@yandex.ru>
2025-07-11 15:19:24 +0700

    added bundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,16 +1,23 @@
 import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HTMLWebpackPlugin({ template: paths.html }),
     new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({ filename: 'css.[contenthash].css', chunkFilename: 'css.[contenthash].css' }),
+    new MiniCssExtractPlugin({
+      filename: 'css.[contenthash].css',
+      chunkFilename: 'css.[contenthash].css',
+    }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }),
   ];
 }
