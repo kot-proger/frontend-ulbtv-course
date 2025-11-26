90fa76bb8e571f630838825680823c9e9b8a3803
kot-proger <babecaleksei@yandex.ru>
2025-11-26 11:58:51 +0700

    redux toolkit lesson

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,17 +1,18 @@
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
+import { DeepPartial } from '@reduxjs/toolkit';
 import { createReduxStore } from '../config/store';
 import { StateScema } from '../config/StateSchema';
 
 interface StoreProviderProps {
   children?: ReactNode;
-  initialState?: StateScema;
+  initialState?: DeepPartial<StateScema>;
 }
 
 const StoreProvider = (props: StoreProviderProps) => {
   const { children, initialState } = props;
 
-  const store = createReduxStore(initialState);
+  const store = createReduxStore(initialState as StateScema);
 
   return <Provider store={store}>{children}</Provider>;
 };
--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,11 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import { counterActions } from '../model/slice/CounterSlice';
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counterValue = useSelector(getCounterValue);
+  const { t } = useTranslation();
 
   const increment = () => {
     dispatch(counterActions.increment());
@@ -18,10 +20,10 @@ const Counter = () => {
     <div>
       <h1 data-testid='value-title'>value = {counterValue}</h1>
       <Button data-testid='increment-btn' onClick={increment}>
-        increment
+        {t('increment')}
       </Button>
       <Button data-testid='decrement-btn' onClick={decrement}>
-        decrement
+        {t('decrement')}
       </Button>
     </div>
   );
