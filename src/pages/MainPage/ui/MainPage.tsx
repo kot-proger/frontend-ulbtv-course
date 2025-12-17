e68fa800b54dc712e22ff51680f6d652a682ebee
kot-proger <babecaleksei@yandex.ru>
2025-12-17 10:49:15 +0700

    .

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -6,32 +6,12 @@ import Input from 'shared/ui/Input/Input';
 import Select from 'shared/ui/Select/Select';
 import cls from './MainPage.module.scss';
 
-/**
- * Главная страница приложения.
- * Отображает счетчик, кнопки, инпут и текущее значение инпута.
- * Также содержит кнопку, которая выводит в консоль текущее значение инпута.
- *
- * @returns {JSX.Element} Компонент главной страницы
- */
 const MainPage = (): JSX.Element => {
   const { t } = useTranslation();
   const [value, setValue] = useState<string>('');
 
-  /**
-   * Обработчик изменения значения инпута.
-   *
-   * @param {string} val Новое значение
-   */
   const onChange = (val: string): void => {
     setValue(val);
-  };
-
-  /**
-   * Выводит текущее значение value в консоль.
-   */
-  const handleLogValue = (): void => {
-    // eslint-disable-next-line no-console
-    console.log(value);
   };
 
   return (
@@ -48,9 +28,6 @@ const MainPage = (): JSX.Element => {
       <Counter />
       <BugButton />
       <Input value={value} onChange={onChange} placeholder='some-input' />
-      <button type='button' onClick={handleLogValue}>
-        {t('Вывести значение в консоль')}
-      </button>
       <span>{value}</span>
     </div>
   );
